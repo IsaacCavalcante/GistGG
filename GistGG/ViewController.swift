597ee9a3720fde64fc77d4397a21b59b414d8306
Isaac <cavalcante.isaac1@gmail.com>
2021-03-31 16:10:39 -0300

    Initial Commit

---
+++ GistGG/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  GistGG
+//
+//  Created by Isaac Cavalcante on 31/03/21.
+//
+
+import UIKit
+import AVFoundation
+import QRCodeReader
+
+class ViewController: UIViewController {
+    
+    lazy var readerVC: QRCodeReaderViewController = {
+        let builder = QRCodeReaderViewControllerBuilder {
+            $0.reader = QRCodeReader(metadataObjectTypes: [.qr], captureDevicePosition: .back)
+            
+            // Configure the view controller (optional)
+            $0.showTorchButton        = false
+            $0.showSwitchCameraButton = false
+            $0.showCancelButton       = false
+            $0.showOverlayView        = true
+            $0.rectOfInterest         = CGRect(x: 0.2, y: 0.2, width: 0.6, height: 0.6)
+        }
+        
+        return QRCodeReaderViewController(builder: builder)
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+//MARK: - QRCodeReaderViewControllerDelegate
+extension ViewController: QRCodeReaderViewControllerDelegate {
+    func reader(_ reader: QRCodeReaderViewController, didScanResult result: QRCodeReaderResult) {
+      reader.stopScanning()
+
+      dismiss(animated: true, completion: nil)
+    }
+
+    //This is an optional delegate method, that allows you to be notified when the user switches the cameraName
+    //By pressing on the switch camera button
+    func reader(_ reader: QRCodeReaderViewController, didSwitchCamera newCaptureDevice: AVCaptureDeviceInput) {
+        print("Switching capture to: \(newCaptureDevice.device.localizedName)")
+    }
+
+    func readerDidCancel(_ reader: QRCodeReaderViewController) {
+      reader.stopScanning()
+
+      dismiss(animated: true, completion: nil)
+    }
+}
+
