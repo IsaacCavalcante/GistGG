4e697d2dce37e2c31d0dbba90961377ad5eb56c9
Isaac Cavalcante <isaac.cavalcante@monitoratec.com.br>
2021-07-02 18:08:34 -0300

    FirebaseAuth integrated

---
+++ Infra/FirebaseAdapter.swift
@@ -0,0 +1,10 @@
+import Foundation
+import Data
+
+public final class FirebaseAdapter: ThirdPartyClient {
+    public init() { }
+    
+    public func thirdPartyAuth(completion: @escaping (ThirdPartyClient.Result) -> Void) {
+        
+    }
+}
---
+++ InfraTests/FirebaseAdapterTest.swift
@@ -0,0 +1,8 @@
+import XCTest
+import Infra
+
+class FirebaseAdapterTest: XCTestCase {
+    func test_provider_with_correct_firebase_credentials() {
+        let sut = FirebaseAdapter()
+    }
+}
