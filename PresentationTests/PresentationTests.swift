7dfbcd5e64471ea859842cf725f7e80d434f7d44
Isaac Cavalcante <isaac.cavalcante@monitoratec.com.br>
2021-07-07 16:06:31 -0300

    ensure SigninPresenter shows unauthorized error if authors closure receive unauthorized error

--- PresentationTests/PresentationTests.swift
+++ PresentationTests/PresentationTests.swift
@@ -72,9 +72,10 @@ class AlertViewSpy: AlertView {
 
 class PresentationTests: XCTestCase {
     func test_sigin_should_show_generic_error_message_if_authentication_fails() {
-        let alertViewSpy = AlertViewSpy()
-        let authenticationSpy = AuthenticationSpy()
-        let sut = SigninPresenter(alertView: alertViewSpy, authentication: authenticationSpy)
+        let test = makeSut()
+        let alertViewSpy = test.alertViewSpy
+        let authenticationSpy = test.authenticationSpy
+        let sut = test.sut
         let exp = expectation(description: "completion to auth remote account should response until 1 second")
         alertViewSpy.observer { viewModel in
             XCTAssertEqual(viewModel, AlertViewModel(title: "Error", message: "Algo inesperado aconteceu. Tente novamente em alguns instantes"))
@@ -83,5 +84,30 @@ class PresentationTests: XCTestCase {
         sut.signin()
         authenticationSpy.completeWithError(.unexpected)
         wait(for: [exp], timeout: 1)
+    }
+    
+    func test_sigin_should_show_unauthorized_message_if_authentication_fails() {
+        let test = makeSut()
+        let alertViewSpy = test.alertViewSpy
+        let authenticationSpy = test.authenticationSpy
+        let sut = test.sut
+        let exp = expectation(description: "completion to auth remote account should response until 1 second")
+        alertViewSpy.observer { viewModel in
+            XCTAssertEqual(viewModel, AlertViewModel(title: "Error", message: "Autenticação de usuário falhou"))
+            exp.fulfill()
+        }
+        sut.signin()
+        authenticationSpy.completeWithError(.unauthorized)
+        wait(for: [exp], timeout: 1)
+    }
+}
+
+extension PresentationTests {
+    func makeSut(file: StaticString = #filePath, line: UInt = #line) -> (sut: SigninPresenter, alertViewSpy: AlertViewSpy, authenticationSpy: AuthenticationSpy) {
+        let alertViewSpy = AlertViewSpy()
+        let authenticationSpy = AuthenticationSpy()
+        let sut = SigninPresenter(alertView: alertViewSpy, authentication: authenticationSpy)
+        
+        return (sut, alertViewSpy, authenticationSpy)
     }
 }
